0cfd904e19b5456eb592cc72d7deac313fc34b10
carlos luna <clunag1800@alumno.ipn.mx>
2026-04-03 14:59:42 -0600

    se agrego las grafica, falta utilizar informacion real

---
+++ frontend/WattGuard-frontend/src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgApexchartsModule } from "ng-apexcharts";
+
+@Component({
+  selector: 'app-home-page',
+  imports: [NgApexchartsModule],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent {
+
+}
